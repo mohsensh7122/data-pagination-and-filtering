78ce2d10a7fee18cfedf6e85b0817a07148b43a1
Mohsen Shirazi <mohsensh4792@gmail.com>
2023-04-19 15:19:42 -0400

    Added the search function

--- js/searchFunction.js
+++ js/searchFunction.js
@@ -6,3 +6,28 @@ const searchEl = `<label for="search" class="student-search">
 </label>`;
 
 header.innerHTML += searchEl;
+
+
+const students = document.querySelectorAll('h3');
+const searchInput = document.getElementById('search');
+
+searchInput.addEventListener('keyup', e => {
+    let inputValue = e.target.value.toLowerCase();
+    console.log(inputValue);
+
+    students.forEach(student => {
+        if(student.textContent.toLowerCase().includes(inputValue)){
+            student.parentNode.parentNode.style.display = 'block';
+            student.parentNode.parentNode.classList.add('match');
+            let newList = document.querySelector('.match');
+            
+        } else {
+            student.parentNode.parentNode.style.display = 'none';
+            student.parentNode.parentNode.classList.remove('match');
+        }
+    })
+
+    // const matches = document.querySelectorAll('.match');
+    // console.log(matches.length);
+    // addPagination(matches)
+})
